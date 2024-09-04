a0f112682323e2f5e4775b64c3f2878f2641e579
Evan Alif Widhyatma <evanalifwidhyatma@gmail.com>
2024-09-04 08:43:58 +0700

    chore: Update libraries and add necessary functions for RTC and SD card logging

--- ESPNOW_RECEIVER/src/ESPNOW_RECEIVER.cpp
+++ ESPNOW_RECEIVER/src/ESPNOW_RECEIVER.cpp
@@ -4,119 +4,154 @@
 #include <ArduinoJson.h>
 #include <StreamUtils.h>
 
-uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+// Libraries for SD card
+#include "FS.h"
+#include "SD.h"
+#include <SPI.h>
 
-// Set your new MAC Address (Receiver MAC Address)
-//uint8_t newMACAddress[] = {0x7C, 0x9E, 0xBD, 0x53, 0x97, 0xF8};
-uint8_t newMACAddress[] = {0xD2, 0x6B, 0x27, 0x1F, 0x58, 0xBE};
-JsonDocument doc_to_espnow; // JSON Doc for Transmitting data to ESPNOW Devices
+// Libraries for RTC
+#include <Wire.h>
+#include <RTClib.h>
 
-String recv_jsondata;
-
+// Define CS pin for the SD card module
+#define SD_CS 5
 #define RXD2 16
 #define TXD2 17
 
-// Create a struct_message called myData
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t newMACAddress[] = {0xD2, 0x6B, 0x27, 0x1F, 0x58, 0xBE};
 
-// ESPNOW Send Callback Function
+String recv_jsondata;
+RTC_DS3231 rtc;
+String dayStamp, timeStamp;
+JsonDocument doc_to_espnow; // JSON Doc for Transmitting data to ESPNOW Devices
+
+void getTimeStamp() {
+  DateTime now = rtc.now();
+  timeStamp = String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second());
+  dayStamp = String(now.day()) + "-" + String(now.month()) + "-" + String(now.year());
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message) {
+  File file = fs.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  file.print(message);
+  file.close();
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+  File file = fs.open(path, FILE_APPEND);
+  if (!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  file.print(message);
+  file.close();
+}
+
+void logSDCard(String data) {
+  String dataMessage = dayStamp + " " + timeStamp + ", " + data + "\r\n";
+  appendFile(SD, "/data.txt", dataMessage.c_str());
+}
+
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
-  JsonDocument doc_from_espnow; // JSON Doc for Receiving data from ESPNOW Devices
-  char* buff = (char*) incomingData;
+  JsonDocument doc_from_espnow;
+  char *buff = (char *)incomingData;
   recv_jsondata = String(buff);
-  Serial.print("Recieved from ESPNOW: "); Serial.println(recv_jsondata);
+  Serial.print("Received from ESPNOW: ");
+  Serial.println(recv_jsondata);
+
   DeserializationError error = deserializeJson(doc_from_espnow, recv_jsondata);
   if (!error) {
-  digitalWrite(2, HIGH);
-      float t1 = doc_from_espnow["t1"];
-      float t2 = doc_from_espnow["t2"];
-      float h1 = doc_from_espnow["h1"];
-      float h2 = doc_from_espnow["h2"];
-      float press = doc_from_espnow["p"];
-      float volt = doc_from_espnow["v"];
-  
-  t1 = t1/100;
-  t2 = t2/100;
-  h1 = h1/100;
-  h2 = h2/100;
-  press  = press/100;
-  volt  = volt/100;
-  //Real Value
-  float temperature = (t1+t2)/2;
-  float humidity = (h1+h2)/2;
+    digitalWrite(2, HIGH);
 
-  double calc = log(humidity / 100.0F) + ((17.625F * temperature) / (243.04F + temperature));
-  double dewpoint = (243.04F * calc / (17.625F - calc));
+    float t1 = doc_from_espnow["t1"];
+    float t2 = doc_from_espnow["t2"];
+    float h1 = doc_from_espnow["h1"];
+    float h2 = doc_from_espnow["h2"];
+    float press = doc_from_espnow["p"];
+    float volt = doc_from_espnow["v"];
 
-  double heat = (0.8*temperature)+((humidity*temperature/500));
+    t1 = t1 / 100;
+    t2 = t2 / 100;
+    h1 = h1 / 100;
+    h2 = h2 / 100;
+    press = press / 100;
+    volt = volt / 100;
 
-      Serial.println(t1);
-      Serial.println(t2);
-      Serial.println(temperature);
-      Serial.println(h1);
-      Serial.println(h2);
-      Serial.println(humidity);
-      Serial.println(dewpoint);
-      Serial.println(press);
-      Serial.println(heat);
-      Serial.println(volt);
-  /*Serial.print("Serailizing to Serial2: ");
-  Serial.println(recv_jsondata);*/
-  Serial.println(len); 
-  JsonDocument doc_to_Serial2;
-  doc_to_Serial2["temp"] = t1;
-  doc_to_Serial2["temp2"] = t2;
-  doc_to_Serial2["humi"] = h1; 
-  doc_to_Serial2["humi2"] = h2;
-  doc_to_Serial2["dew"] = dewpoint;
-  doc_to_Serial2["heat"] = heat;
-  doc_to_Serial2["press"] = press;
-  doc_to_Serial2["volt"] = volt;
-  WriteBufferingStream bufferedSerial2(Serial2, 64);        
-  serializeJson(doc_to_Serial2, Serial2);            // Writing Data to Serial2
-  bufferedSerial2.flush();
-  digitalWrite(2, LOW);
+    float temperature = (t1 + t2) / 2;
+    float humidity = (h1 + h2) / 2;
 
+    double calc = log(humidity / 100.0F) + ((17.625F * temperature) / (243.04F + temperature));
+    double dewpoint = (243.04F * calc / (17.625F - calc));
+    double heat = (0.8 * temperature) + ((humidity * temperature / 500));
+
+    Serial.println(t1);
+    Serial.println(t2);
+    Serial.println(temperature);
+    Serial.println(h1);
+    Serial.println(h2);
+    Serial.println(humidity);
+    Serial.println(dewpoint);
+    Serial.println(press);
+    Serial.println(heat);
+    Serial.println(volt);
+
+    getTimeStamp();
+    String data = String(t1) + ", " + String(t2) + ", " + String(h1) + ", " + String(h2) + ", " + String(dewpoint) + ", " + String(heat) + ", " + String(press) + ", " + String(volt);
+    logSDCard(data);
+
+    Serial.println("Data logged to SD card.");
+
+    JsonDocument doc_to_Serial2;
+    doc_to_Serial2["temp"] = t1;
+    doc_to_Serial2["temp2"] = t2;
+    doc_to_Serial2["humi"] = h1;
+    doc_to_Serial2["humi2"] = h2;
+    doc_to_Serial2["dew"] = dewpoint;
+    doc_to_Serial2["heat"] = heat;
+    doc_to_Serial2["press"] = press;
+    doc_to_Serial2["volt"] = volt;
+
+    WriteBufferingStream bufferedSerial2(Serial2, 64);
+    serializeJson(doc_to_Serial2, Serial2);
+    bufferedSerial2.flush();
+
+    digitalWrite(2, LOW);
   } else {
-  Serial.print(F("deserializeJson() failed: "));
-  Serial.println(error.f_str());
-  return;
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
   }
 }
- 
+
 void setup() {
-  // Initialize Serial Monitor
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXD2, TXD2);
 
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
-
-  // ESP32 Board add-on after version > 1.0.5
   esp_wifi_set_mac(WIFI_IF_STA, &newMACAddress[0]);
 
   pinMode(2, OUTPUT);
 
-  // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  // Register the send and receive callback functions
-  esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
-  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
 
-  // Add peer information for broadcast
-  esp_now_peer_info_t peerInfo;
-  memset(&peerInfo, 0, sizeof(peerInfo));
-  for (int ii = 0; ii < 6; ++ii ) {
-    peerInfo.peer_addr[ii] = (uint8_t) broadcastAddress[ii];
-  }
+  esp_now_peer_info_t peerInfo = {};
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
 
@@ -124,24 +159,39 @@ void setup() {
     Serial.println("Failed to add peer");
     return;
   }
-}
- 
-void loop() {
- if (Serial2.available()) {
-    // Recieving data (JSON) from BLYNK ESP
-    String recv_str_jsondata = Serial2.readStringUntil('\n');
 
-    //Serializing JSON
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  if (!SD.begin(SD_CS)) {
+    Serial.println("ERROR - SD card initialization failed!");
+    return;
+  }
+
+  File file = SD.open("/data.txt");
+  if (!file) {
+    writeFile(SD, "/data.txt", "Timestamp, Temp1, Temp2, Humidity1, Humidity2, Dew Point, Heat Index, Pressure, Voltage \r\n");
+  }
+  file.close();
+}
+
+void loop() {
+  if (Serial2.available()) {
+    String recv_str_jsondata = Serial2.readStringUntil('\n');
     serializeJson(doc_to_espnow, recv_str_jsondata);
     Serial.println(recv_str_jsondata);
 
-    // Broadcasting data (JSON) via ESP-NOW
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) recv_str_jsondata.c_str(), sizeof(recv_str_jsondata) * recv_str_jsondata.length());
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *)recv_str_jsondata.c_str(), sizeof(recv_str_jsondata) * recv_str_jsondata.length());
 
     if (result == ESP_OK) {
       Serial.println("Sent with success");
-    }
-    else {
+    } else {
       Serial.println(result);
     }
   }
